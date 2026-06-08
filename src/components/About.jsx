98aa47e22f1377502045c71d2cdf6800d0179fb6
movindu477 <peshalaweerabahu0@gmail.com>
2026-06-08 18:17:03 +0530

    Issue fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Download, CheckCircle, Briefcase, Layers, Code, Sparkles, ExternalLink, Globe, Smartphone, Database } from "lucide-react";
-import meImage from "../assets/about.jpg";
+import meImage from "../assets/about.JPG";
 import resumeFile from "../assets/Movindus_CV.pdf";
 
 const CountUp = ({ end, duration = 2000 }) => {
